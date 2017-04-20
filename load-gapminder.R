9939f80d9581a63958db18a406e71e6a7c0b1b00
lonsbio <andrew.lonsdale@lonsbio.com.au>
2017-04-20 22:59:02 +1000

    Parameterise file and URL

--- load-gapminder.R
+++ load-gapminder.R
@@ -1,5 +1,7 @@
 # Download the Gapminder data set
 
+file_location <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv"
+output_location <- "data/gapminder-FiveYearData.csv"
 
-download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "data/gapminder-FiveYearData.csv")
-gapminder <- read.csv(file = "data/gapminder-FiveYearData.csv")
+download.file(file_location, destfile = output_location)
+gapminder <- read.csv(file = output_location)
