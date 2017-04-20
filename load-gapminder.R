8f1ea79d9cd720e756b17e28e2c1320e528a3b4e
lonsbio <andrew.lonsdale@lonsbio.com.au>
2017-04-20 23:02:48 +1000

    Downoad only if file does not exist

--- load-gapminder.R
+++ load-gapminder.R
@@ -3,5 +3,9 @@
 file_location <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv"
 output_location <- "data/gapminder-FiveYearData.csv"
 
-download.file(file_location, destfile = output_location)
+if (!file.exists(output_location)) {
+  download.file(file_location, destfile = output_location)
+}
+
 gapminder <- read.csv(file = output_location)
+
